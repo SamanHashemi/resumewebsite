43ca2a0f3cb7ea2305ef001b5d466319271a8bb5
SamanHashemi <saman.hpour@gmail.com>
2020-06-23 23:48:30 -0700

    Added proper index path

--- server.js
+++ server.js
@@ -130,6 +130,6 @@ if (process.env.NODE_ENV === 'production') {
   // Express serve up index.html file if it doesn't recognize route
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve('./public/index.html'));
   });
 }
