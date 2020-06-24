b34f8d0b6f2395b82d7e0c6daeefd16b080a251f
SamanHashemi <saman.hpour@gmail.com>
2020-06-23 23:41:54 -0700

    Port Number Changed

--- server.js
+++ server.js
@@ -115,6 +115,14 @@ router.post('/send', (req, res, next) => {
   })
 })
 
+
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use('/', router)
+app.listen(process.env.PORT || 5000)
+
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
   app.use(express.static('client/build'));
@@ -125,9 +133,3 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
-
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use('/', router)
-app.listen(process.env.PORT || 5000)
